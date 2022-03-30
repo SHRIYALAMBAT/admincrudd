afd5f453d510785886dbb28f860920fc203b56aa
Shriya Lambat <shriyalambat@gmail.com>
2022-03-30 15:28:20 +0530

    Create Admin.js

---
+++ Page/Admin.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../Components/Nav";
+import Cards from "../Components/Cards"
+import Footer from "../Components/Footer"
+const Admin = () => {
+  return (
+    <div>
+      <Nav />
+      <Cards></Cards>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Admin;
